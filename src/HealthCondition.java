64adb9a901c0a08cece86eb43e163f51d6ae79ff
Mohammad Saeed <saeed3.1417@gmail.com>
2025-04-14 09:04:32 +0600

    added health condition getters

---
+++ src/HealthCondition.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class HealthCondition {
+
+    private int sleepHours;
+    private int meals;
+    private boolean dizzy;
+    private boolean tummyPain;
+    private boolean chestPain;
+
+    // Constructor to initialize the health condition
+    public HealthCondition(int sleepHours, int meals, boolean dizzy, boolean tummyPain, boolean chestPain) {
+        this.sleepHours = sleepHours;
+        this.meals = meals;
+        this.dizzy = dizzy;
+        this.tummyPain = tummyPain;
+        this.chestPain = chestPain;
+    }
+
+    // Getters
+    public int getSleepHours() {
+        return sleepHours;
+    }
+
+    public int getMeals() {
+        return meals;
+    }
+
+    public boolean isDizzy() {
+        return dizzy;
+    }
+
+    public boolean isTummyPain() {
+        return tummyPain;
+    }
+
+    public boolean isChestPain() {
+        return chestPain;
+    }
+}
